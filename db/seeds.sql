6242dd7d729d5416bb43a01a54eadb6898fe0841
Bryan Jerger <Bjerger1470@gmail.com>
2020-01-28 14:43:52 -0600

    db creation and server setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', false);
